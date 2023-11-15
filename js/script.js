29fd0d37fe9ae7ede345150d2ebb981a13f3d8ac
Guillaume LAMIC <75310445+Troncar78@users.noreply.github.com>
2023-11-15 09:34:22 +0100

    Update script.js
    Pop up et récupération de la sélection utilisateur

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
-const texteAudio = document.getElementById('texte-audio');
-const audioDescription = document.getElementById('audio-description');
+window.addEventListener('mouseup', (event) => {
+    //recuperation de la sélection
+    const selectedText = window.getSelection().toString();
 
-texteAudio.addEventListener('mouseup', function() {
-  const selectedText = window.getSelection().toString();
-  if (selectedText.trim() !== '') {
-    // Mettre à jour la source audio avec l'audio description appropriée
-    audioDescription.src = './source/Clapping Sound Effects.mp3';
-    audioDescription.load();
-    audioDescription.play();
-  }
+    //récupération de l'id de la balise sélectionné
+    const targetElement = event.target;
+    const balise = targetElement.tagName;
+    const id = targetElement.id;
+
+    //Vérification du text selectionner 
+    if (selectedText !== '') {
+        popUp(id);
+    } else {
+        console.log("Aucune sélection d'élément effectuée.");
+    }
 });
+
+//fonction pour afficher la pop up 
+function popUp (selectedId) {
+    //donnez la classe a la target du selectedText
+    const targetElement = document.getElementById(selectedId);
+    targetElement.classList.add("popup");
+
+    //affichage de la pop up
+    const spanBtn = document.createElement("span");
+    spanBtn.innerHTML = '<button type="button"><img src="https://www.svgrepo.com/show/521855/sound.svg" alt="" srcset="" id="imgBtn"/></button>';
+    spanBtn.className = 'popuptext';
+    spanBtn.id = 'myPopup';
+    document.getElementById(selectedId).appendChild(spanBtn);
+    const popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
