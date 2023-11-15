7be2893ad6e84ed655d65630b184a5de60515944
Guillaume LAMIC <75310445+Troncar78@users.noreply.github.com>
2023-11-15 10:31:19 +0100

    Update script.js
    Version finale

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
-window.addEventListener('mouseup', (event) => {
+document.addEventListener('mouseup', (event) => {
     //recuperation de la sélection
     const selectedText = window.getSelection().toString();
 
-    //récupération de l'id de la balise sélectionné
-    const targetElement = event.target;
-    const balise = targetElement.tagName;
-    const id = targetElement.id;
-
     //Vérification du text selectionner 
     if (selectedText !== '') {
+        //récupérer l'id du texte sélectionné
+        const targetElement = event.target;
+        const id = targetElement.id;
+        //récupération de l'id de la balise sélectionné
         popUp(id);
+        //Target du bouton
+        const targetBtn = document.getElementById('popupBtn');
+        //Verif si le bouton existe bien + event si bouton appuyé
+        if (targetBtn){
+            targetBtn.addEventListener('mousedown',() => {
+                //Api, fetch
+                console.log('gg');
+            })
+        } else {
+            console.log('pas de bouton')
+        }
     } else {
         console.log("Aucune sélection d'élément effectuée.");
     }
@@ -17,13 +27,13 @@ window.addEventListener('mouseup', (event) => {
 
 //fonction pour afficher la pop up 
 function popUp (selectedId) {
+    console.log(selectedId);
     //donnez la classe a la target du selectedText
     const targetElement = document.getElementById(selectedId);
     targetElement.classList.add("popup");
-
     //affichage de la pop up
     const spanBtn = document.createElement("span");
-    spanBtn.innerHTML = '<button type="button"><img src="https://www.svgrepo.com/show/521855/sound.svg" alt="" srcset="" id="imgBtn"/></button>';
+    spanBtn.innerHTML = '<button type="button" id="popupBtn"><img src="https://www.svgrepo.com/show/521855/sound.svg" alt="" srcset="" id="imgBtn"/></button>';
     spanBtn.className = 'popuptext';
     spanBtn.id = 'myPopup';
     document.getElementById(selectedId).appendChild(spanBtn);
