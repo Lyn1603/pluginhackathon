9388be7a2150f3b856d7880febf469ce4baf6223
Guillaume LAMIC <75310445+Troncar78@users.noreply.github.com>
2023-11-15 15:00:31 +0100

    Update script.js
    text to speech works

--- js/script.js
+++ js/script.js
@@ -1,25 +1,24 @@
 document.addEventListener('mouseup', (event) => {
-    
+    let selectedText = "";
     //recuperation de la sélection
-    const selectedText = window.getSelection().toString();
+    selectedText = window.getSelection().toString();
 
     //Vérification du text selectionner 
     if (selectedText !== '') {
-        
         //récupérer l'id du texte sélectionné
         const targetElement = event.target;
         const id = targetElement.id;
-
+        //récupération de l'id de la balise sélectionné
         popUp(id);
-        
         //Target du bouton
         const targetBtn = document.getElementById('popupBtn');
-        
         //Verif si le bouton existe bien + event si bouton appuyé
         if (targetBtn){
             targetBtn.addEventListener('mousedown',() => {
                 //Api, fetch
-                console.log('gg');
+                console.log(selectedText);
+                responsiveVoice.speak(selectedText, "French Female");
+                removePopUp();
             })
         } else {
             console.log('pas de bouton')
@@ -27,6 +26,7 @@ document.addEventListener('mouseup', (event) => {
     } else {
         console.log("Aucune sélection d'élément effectuée.");
     }
+    console.log('end')
 });
 
 //fonction pour afficher la pop up 
@@ -34,7 +34,6 @@ function popUp (selectedId) {
     //donnez la classe a la target du selectedText
     const targetElement = document.getElementById(selectedId);
     targetElement.classList.add("popup");
-    
     //affichage de la pop up
     const spanBtn = document.createElement("span");
     spanBtn.innerHTML = '<button type="button" id="popupBtn"><img src="https://www.svgrepo.com/show/521855/sound.svg" alt="" srcset="" id="imgBtn"/></button>';
@@ -43,4 +42,11 @@ function popUp (selectedId) {
     document.getElementById(selectedId).appendChild(spanBtn);
     const popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
+}
+
+function removePopUp(){
+    const element = document.getElementById("popupBtn");
+    element.remove();
+    const element2 = document.getElementById("myPopup");
+    element2.remove();
 }
